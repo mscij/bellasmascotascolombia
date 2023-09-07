661fa24fcec49aa5996177e0cbd6d3380e92e65b
jhonsqo <144387824+jhonsqo@users.noreply.github.com>
2023-09-07 17:07:02 -0500

    commit

---
+++ search.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="js/jquery.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="js/superfish.js"></script>
+        <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+         
+        <script type="text/javascript" src="search/search.js"></script>
+  <!--[if lt IE 8]>
+      <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]>
+     
+    <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<div class="main">  
+  <header>  
+    <div class="container">
+      <div class="row">
+        <div class="span12 clearfix">
+          <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt="Dogs"></a></h1> 
+          <div class="navbar navbar_ clearfix">
+            <div class="navbar-inner">                                                 
+              <div class="nav-collapse nav-collapse_ collapse">
+                <ul class="nav sf-menu clearfix">
+                  <li><a href="index.html">HOME</a></li>
+                  <li class="sub-menu"><a href="index-1.html">BREEDS</a>
+                  	<ul>
+                      <li><a href="#">Lorem ipsum dolor </a></li>
+                      <li><a href="#">Sit amet conse</a></li>
+                      <li><a href="#">Ctetur adipisicing</a></li>
+                      <li><a href="#">Elit sed do</a></li>
+                      <li><a href="#">Eiusmod tempor</a></li>
+                      <li><a href="#">Incididunt ut</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="index-2.html">TRAINING</a></li>
+                  <li><a href="index-3.html">ABOUT US</a></li>
+                  <li><a href="index-4.html">CONTACTS</a></li>
+                </ul>
+              </div>
+            </div>
+          </div> 
+        </div> 
+      </div> 
+    </div>  
+  </header>  
+  <!--==============================content=================================-->
+  <div id="content">
+    <div class="container">
+        	<div class="row">
+          		<div class="span12">
+               		<h3>Search result:</h3>
+               		<div id="search-results"></div>
+          		</div>
+         	</div>      
+       </div>
+  </div>    
+<!--==============================footer=================================-->
+  <footer>
+      <div class="container">
+         <div class="row">
+            <div class="span12">Dogs  &copy; 2013&nbsp;&nbsp;&nbsp;<a href="index-5.html">Privacy Policy</a></div>
+         </div>  
+      </div>
+    </footer>
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
